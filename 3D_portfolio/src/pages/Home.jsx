1e4a552a325e49093c3fc69a8f8891cb30772c7a
Rockey C.R <rockeyrai234@gmail.com>
2024-11-08 17:09:08 +0545

    adding lighting to the island

--- 3D_portfolio/src/pages/Home.jsx
+++ 3D_portfolio/src/pages/Home.jsx
@@ -4,6 +4,24 @@ import Loader from '../components/Loader'
 import { Island } from '../models/Island'
 
 const Home = () => {
+
+  const adjustIslandForScreenSize=()=>{
+    let screenScale=null;
+    let screenPostion=[0, -6.5, -43];
+    let rotation =[0.1,4.7,0]
+
+    if(window.innerWidth < 768){
+      screenScale=[0.9,0.9,0.9];
+    }else{
+      screenScale=[1,1,1]
+    }
+
+    return [screenScale,screenPostion,rotation]
+  }
+
+  const [islandScale,islandPosition,islandRotation]=adjustIslandForScreenSize();
+
+
   return (
     <section className='w-full h-screen relative'>
       {/* <div className="absolute top-28 left-0 z-10 flex items-center justify-center"> 
@@ -13,11 +31,15 @@ const Home = () => {
       camera={{near: 0.1,far: 1000}}
       >
       <Suspense fallback={<Loader/>}>
-      <directionalLight/>
-      <ambientLight/>
-      <spotLight/>
-      <hemisphereLight/>
-      <Island/>
+      <directionalLight position={[1,1,1]} intensity={1}/>
+      <ambientLight intensity={0.2}/>
+      {/* <spotLight /> */}
+      <hemisphereLight skyColor="#b1e1ff" groundColor='#000000' intensity={1} />
+      <Island
+        position={islandPosition}
+        scale={islandScale}
+        rotation={islandRotation}
+      />
       </Suspense>
 
       </Canvas>
