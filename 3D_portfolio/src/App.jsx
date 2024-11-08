22a3c5ec814fb4f0bbe024dfd19d97994f46304e
Rockey C.R <rockeyrai234@gmail.com>
2024-11-08 07:35:54 +0545

    setup complete for the 3D Portfolio

---
+++ 3D_portfolio/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  console.log('hello')
+  return (
+    <div>
+      <h1  className='text-red-500 bold text-xl underline'>hello</h1>
+      <div className=''>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
