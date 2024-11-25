1e783632de0ad4cb1f35a1647b4bb8acfe9cf0d8
Rockey C.R <rockeyrai234@gmail.com>
2024-11-25 14:41:29 +0545

    installign react-router-dom and chainging vit.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom'],
+    },
+  },
   assetsInclude:['**/*.glb']
 })
